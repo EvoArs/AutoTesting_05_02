1a913c85e4f4b1f3c6b30d617a6a8f351897d201
EvoArs <EvoyanAG@mail.ru>
2023-09-01 16:09:24 +0300

    add test

---
+++ src/test/java/ru/netology/test/TestLoginFunction.java
@@ -0,0 +1,79 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.data.DataGenerator.Registration.getUser;
+import static ru.netology.data.DataGenerator.getRandomLogin;
+import static ru.netology.data.DataGenerator.getRandomPassword;
+
+public class TestLoginFunction {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    //Позитивный тест входа в систему с зарегистрированным пользователем
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessFulLoginIfRegisteredActiveUser() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id=login] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(registeredUser.getPassword());
+        $("[data-test-id=action-login] .button__content").click();
+        $("div#root").shouldBe(Condition.text("Личный кабинет"));
+    }
+
+    // Негативный тест входа в систему с незарегистрированным пользователем
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        $("[data-test-id=login] .input__control").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(notRegisteredUser.getPassword());
+        $("[data-test-id=action-login] .button__content").click();
+        $("[data-test-id=error-notification] .notification__content").shouldBe(Condition.text("Ошибка! " + "Неверно указан логин или пароль"), Duration.ofSeconds(10));
+    }
+
+    // Негативный тест входа в систему с заблокированным пользователем
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        $("[data-test-id=login] .input__control").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(blockedUser.getPassword());
+        $("[data-test-id=action-login] .button__content").click();
+        $("[data-test-id=error-notification] .notification__content").shouldBe(Condition.text("Ошибка! " + "Пользователь заблокирован"));
+    }
+
+    // Негативный тест входа в систему с неправильным логином
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        $("[data-test-id=login] .input__control").setValue(wrongLogin);
+        $("[data-test-id=password] .input__control").setValue(registeredUser.getPassword());
+        $("[data-test-id=action-login] .button__content").click();
+        $("[data-test-id=error-notification] .notification__content").shouldBe(Condition.text("Ошибка! " + "Неверно указан логин или пароль"), Duration.ofSeconds(10));
+    }
+
+    // Негативный тест входа с неправильным паролем
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        $("[data-test-id=login] .input__control").setValue(registeredUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(wrongPassword);
+        $("[data-test-id=action-login] .button__content").click();
+        $("[data-test-id=error-notification] .notification__content").shouldBe(Condition.text("Ошибка! " + "Неверно указан логин или пароль"), Duration.ofSeconds(10));
+    }
+}
